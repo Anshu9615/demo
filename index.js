74dcbb2599c57037567723fffd2d77b677e4a7d4
Anshu9615 <118248318+Anshu9615@users.noreply.github.com>
2023-08-04 16:40:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // filename: lambda_function.js
 exports.handler = async (event, context) => {
-    const message = "Hello from your Lambda function , it an update!";
+    const message = "Hello from your Lambda function , lets do an update!";
     console.log(message);
     return {
         statusCode: 200,
