167c6e5d5def60e3123572e73f7f0f7e5664c4ed
anshu kumar <anshu.kumar@yendigital.com>
2023-08-04 12:59:45 +0530

    lambda function

---
+++ index.js
@@ -0,0 +1,9 @@
+// filename: lambda_function.js
+exports.handler = async (event, context) => {
+    const message = "Hello from your Lambda function!";
+    console.log(message);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(message)
+    };
+};
